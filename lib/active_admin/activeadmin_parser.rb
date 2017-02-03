aab6533d0a76468e9cb805b213ae470cd1f5322e
Denis Presnov <torq07@gmail.com>
2017-02-03 15:40:38 +0200

    test commit

---
+++ lib/active_admin/activeadmin_parser.rb
@@ -0,0 +1,16 @@
+require "activeadmin"
+require "active_admin/activeadmin_parser/version"
+
+module ActiveAdmin
+
+	module ActiveadminParser
+	  
+	  def parser_testing
+	  	
+	  end
+
+	end
+
+end
+
+ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::ActiveadminParser
