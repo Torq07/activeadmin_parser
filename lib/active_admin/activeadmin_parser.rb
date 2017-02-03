b16e4cc3e84d201d2e8be3b038705ceb5c2f3b02
Denis Presnov <torq07@gmail.com>
2017-02-03 16:09:09 +0200

    0.1.1

--- lib/active_admin/activeadmin_parser.rb
+++ lib/active_admin/activeadmin_parser.rb
@@ -1,20 +1,19 @@
 require "activeadmin"
-require "activeadmin"
 require "active_admin/activeadmin_parser/version"
 
 module ActiveAdmin
+	
 	module ActiveadminParser
 	  
-	  extend ActiveSupport::Concern
-
 	  def parser_testing
 	  	puts 'Testing parser'
 	  end
 
 	end
 	
-	ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::ActiveadminParser
 
 end
+
+::ActiveAdmin::DSL.send	:include, ActiveAdmin::ActiveadminParser
 
 
